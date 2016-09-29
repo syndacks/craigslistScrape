546659797e4ca8c7146d6a98f8b790778c1e9ba2
Dacks <math.milliken@gmail.com>
2016-09-28 21:06:10 -0700

    commenting out main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,14 @@
+//immediately invoked function expression (IIFE)
 $(function(){
+	// CALL BACK 1
  $('#search').on('keyup', function(e){
+ 	//if user hits Enter key
    if(e.keyCode === 13) {
+   	// set the search value equal to 'parameters',
+   	// which is an object we pass back to server side
      var parameters = { search: $(this).val() };
+    	 // CALL BACK 2
+    	 //this is the actual AJAX request
        $.get( '/searching',parameters, function(data) {
        $('#results').html(data);
      });
