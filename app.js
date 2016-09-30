c3a7516d6729a237caade14bba4b56e4a9a90879
Syndacks <math.milliken@gmail.com>
2016-09-30 10:42:39 -0700

    added handlebars template

--- app.js
+++ app.js
@@ -14,30 +14,43 @@ app.get("/", function(req, res){
 });
 
 app.get('/searching', function(req, res){
-
 	 // input value from search
 	 var val = req.query.search;
-
 	// url used to search yql
 	var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search" +
-	"%20where%20location%3D%22sfbay%22%20and%20type%3D%22jjj%22%20and%20query%3D%22" + val + "%22&format=" +
+	"%20where%20location%3D%22sfbay%22%20and%20type%3D%22bia%22%20and%20query%3D%22" + val + "%22&format=" +
 	"json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-	console.log(url);
 
-	request(url, function(err, resp, body){
-		body = JSON.parse(body);
-		console.log(body);
-		//logic used to compare search results with the input from user
-		if(!body.query.results.RDF.item){
-			craig = "No results found. Try again.";
-		} else {
-			results = body.query.results.RDF.item[0]['about'];
-			// console.log(craig);
-			craig = '<a href="'+results+'">'+results+'</a>'
-		}
-			res.send(craig);
+	requests(url, function(data){
+		res.send(data);
 	});
 });
+
+
+function requests(url, callback){
+	//request module processes the yql url and return the results in JSON
+	request(url, function(err, resp, body){
+		var resultsArray = [];
+		//use request to parse the body of the URL into JSON
+		body = JSON.parse(body);
+		console.log(body.query.results.RDF.item);
+		//logic used to compare search results with the input from user
+		if(!body.query.results.RDF.item){
+			results = "No results found. Try again.";
+			callback(results);
+		} else {
+			results = body.query.results.RDF.item;
+			for(var i = 0; i<results.length; i++){
+				resultsArray.push(
+					{title:results[i].title[0], about:results[i]["about"], desc:results[i]["description"]}
+				);
+			};
+		};
+		console.log(resultsArray);
+		//pass back the results to client side
+		callback(resultsArray);
+	});
+};
 
 app.listen(3000, function(err){
 	if(err){
