c512aa5aace39475b0ebb080c5e0060428dcae70
Dacks <math.milliken@gmail.com>
2016-09-29 20:35:27 -0700

    not much

--- app.js
+++ app.js
@@ -15,36 +15,28 @@ app.get("/", function(req, res){
 
 app.get('/searching', function(req, res){
 
- // input value from search
- var val = req.query.search;
-//console.log(val);
+	 // input value from search
+	 var val = req.query.search;
 
-// url used to search yql
-var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search" +
-"%20where%20location%3D%22sfbay%22%20and%20type%3D%22jjj%22%20and%20query%3D%22" + val + "%22&format=" +
-"json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-console.log(url);
+	// url used to search yql
+	var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search" +
+	"%20where%20location%3D%22sfbay%22%20and%20type%3D%22jjj%22%20and%20query%3D%22" + val + "%22&format=" +
+	"json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+	console.log(url);
 
-request(url, function(err, resp, body){
-	body = JSON.parse(body);
-	console.log(body);
-	//logic used to compare search results with the input from user
-	if(!body.query.results.RDF.item){
-		craig = "No results found. Try again.";
-	} else {
-		results = body.query.results.RDF.item[0]['about'];
-		// console.log(craig);
-		craig = '<a href="'+results+'">'+results+'</a>'
-	}
-		res.send(craig);
-});
-
-	//pass back the results to client side
-
-
-// testing the route
-// res.send("WHEEE");
-
+	request(url, function(err, resp, body){
+		body = JSON.parse(body);
+		console.log(body);
+		//logic used to compare search results with the input from user
+		if(!body.query.results.RDF.item){
+			craig = "No results found. Try again.";
+		} else {
+			results = body.query.results.RDF.item[0]['about'];
+			// console.log(craig);
+			craig = '<a href="'+results+'">'+results+'</a>'
+		}
+			res.send(craig);
+	});
 });
 
 app.listen(3000, function(err){
